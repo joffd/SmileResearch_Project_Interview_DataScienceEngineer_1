c1eacd7c0a6c69163f9eba9eb3445918841e1979
joffrey.dalet@smiletech.io <joffrey.dalet@smiletech.io>
2021-02-18 19:30:03 +0800

    Changed to use Deedle

--- Product.fsx
+++ Product.fsx
@@ -2,12 +2,13 @@
 #r "nuget: FSharp.Stats"
 // third party .net packages 
 #r "nuget: Plotly.NET, 2.0.0-beta5"
+#r "nuget: Plotly.NET.Interactive, 2.0.0-beta5"
 #r "nuget: FSharp.Data"
 #I "data"
 
 open System
 open Deedle
-
+open Plotly.NET
 /// DOMAIN
 
 type ProductDetails = {
@@ -27,20 +28,51 @@ type Reset = {
 
 type Resets = List<Reset>
 
+type ResetsTS = Series<DateTimeOffset, float>
+
 type Holding = {
     Product: ProductDetails
-    Resets: Resets
+    Resets: ResetsTS
 } with
-    member this.addReset (reset: Reset) =
-        { this with Resets = reset :: this.Resets}
-    member this.addResets (resets: Resets) =
-        { this with Resets = resets @ this.Resets}
+    member this.AddReset ((dt,close): DateTimeOffset * float) =
+        let newResets =
+            series [dt,close]
+            |> Series.filter (fun k _ -> k >= this.Product.StartDate && k <= this.Product.EndDate)
+            |> Series.sortByKey
+        { this with 
+            Resets = 
+                Series.mergeUsing UnionBehavior.PreferRight this.Resets newResets
+                |> Series.sortByKey }
+    member this.AddResets (resets: ResetsTS) =
+        let newResets =
+            resets
+            |> Series.filter (fun k _ -> k >= this.Product.StartDate && k <= this.Product.EndDate)
+        { this with 
+            Resets = 
+                Series.mergeUsing UnionBehavior.PreferRight this.Resets newResets
+                |> Series.sort }
     
     static member Create(product: ProductDetails, initLevel) =
         {
             Product = product
-            Resets = [{DateTime = product.StartDate ; ResetIndex = initLevel}]
+            Resets = series [product.StartDate, initLevel]
         }
+
+// type Holding = {
+//     Product: ProductDetails
+//     Resets: Resets
+// } with
+//     member this.addReset (reset: Reset) =
+//         { this with Resets = reset :: this.Resets}
+//     member this.addResets (resets: Resets) =
+//         { this with Resets = resets @ this.Resets}
+    
+//     static member Create(product: ProductDetails, initLevel) =
+//         {
+//             Product = product
+//             Resets = [{DateTime = product.StartDate ; ResetIndex = initLevel}]
+//         }
+
 
 /// CONST
 
@@ -68,8 +100,9 @@ let createProduct (startD: DateTimeOffset) (nb: int) (gammaD: float) (thetaD: fl
 // Complixity: O(n)
 let calcRealisedGamma (h: Holding) =
     h.Resets
-    |> List.pairwise
-    |> List.sumBy (fun (x,y) -> 50. * h.Product.GammaD * pown (((y.ResetIndex - x.ResetIndex) / x.ResetIndex)) 2)
+    |> Series.sortByKey
+    |> Series.pairwiseWith (fun _ (x,y) -> 50. * h.Product.GammaD * pown (((y - x) / x)) 2)
+    |> Stats.sum
 
 // Calc Realised PnL
 let calcRealisedPnl (h: Holding) =
@@ -105,7 +138,15 @@ newFrame.Print()
 let closes = newFrame?Close
 let closesMidnight = closes |> Series.filter (fun k _ -> k.TimeOfDay = TimeSpan(0,0,0))
 
-
+let getStartingDatesPossible (length: int)=
+    let lastDt =
+        closesMidnight
+        |> Series.keys
+        |> Seq.max
+    closesMidnight
+    |> Series.keys
+    |> Seq.filter (fun x -> x.AddDays(float length) < lastDt)
+    
 // Charting Price
 
 
@@ -117,19 +158,50 @@ let nbDays = 5
 
 let product1 = createProduct (DateTimeOffset.UtcNow) nbDays gammaD thetaD
 
-let rnd = System.Random(Seed = 0)
-let randoms = [for i in 0..4 do yield rnd.NextDouble()]
+
 
 
 let resets =
     List.init 5 (fun i -> DateTimeOffset.UtcNow.AddHours(float (8 * i)), float i)
-    |> List.map (fun (d,i) -> {DateTime = d ; ResetIndex = (50000. * (1.0 + i * 0.05)) |> round})
+    |> List.map (fun (d,i) ->  d, (50000. * (1.0 + i * 0.05)) |> round)
+    |> series
 
 let initH = Holding.Create(product1, 50000.)
 
-let calcRGamma = calcRealisedGamma (initH.addResets(resets))
+let calcRGamma = calcRealisedGamma (initH.AddResets(resets))
 
-let calcRPnl = calcRealisedPnl (initH.addResets(resets))
+let calcRPnl = calcRealisedPnl (initH.AddResets(resets))
 DateTimeOffset.UtcNow.AddHours(33.)
 |> calcRPnl
 
+/// 
+let allPossibleProduct =
+    getStartingDatesPossible 30
+    |> Seq.toList
+    |> List.map (fun x -> createProduct x 30 gammaD thetaD)
+    |> List.map (fun x -> Holding.Create(x, closesMidnight |> Series.get(x.StartDate)))
+
+let simpleEodReset =
+    allPossibleProduct
+    |> List.map (fun x -> x.AddResets(closesMidnight))
+    |> List.map (fun x -> x.Product.StartDate, calcRealisedPnl x x.Product.EndDate)
+    |> series
+let avg, median = simpleEodReset |> Stats.mean, simpleEodReset |> Stats.median
+let seriesToSeq (x:Series<DateTimeOffset,float>)=
+    Seq.zip (x.Keys |> Seq.map (fun x -> x.UtcDateTime)) x.Values
+
+
+simpleEodReset
+|> seriesToSeq
+|> Chart.Line
+|> Chart.Show
+
+closesMidnight
+|> Series.pairwiseWith (fun _ (v1,v2) -> v2 - v1)
+|> seriesToSeq
+|> Chart.Bar
+|> Chart.Show
+
+
+allPossibleProduct
+|> List.head
