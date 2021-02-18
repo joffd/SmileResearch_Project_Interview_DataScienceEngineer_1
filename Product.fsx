baf054d771451c16a95af67efbfb0608bda616a4
joffrey.dalet@smiletech.io <joffrey.dalet@smiletech.io>
2021-02-18 14:30:22 +0800

    Added data + update to model

--- Product.fsx
+++ Product.fsx
@@ -1,6 +1,14 @@
+#r "nuget: Deedle"
+#r "nuget: FSharp.Stats"
+// third party .net packages 
+//#r "nuget: Plotly.NET, 2.0.0-beta5"
+#r "nuget: FSharp.Data"
 
 
 open System
+
+
+/// DOMAIN
 
 type ProductDetails = {
     StartDate: DateTimeOffset
@@ -17,8 +25,29 @@ type Reset = {
     ResetIndex: float
 }
 
+type Resets = List<Reset>
+
+type Holding = {
+    Product: ProductDetails
+    Resets: Resets
+} with
+    member this.addReset (reset: Reset) =
+        { this with Resets = reset :: this.Resets}
+    member this.addResets (resets: Resets) =
+        { this with Resets = resets @ this.Resets}
+    
+    static member Create(product: ProductDetails, initLevel) =
+        {
+            Product = product
+            Resets = [{DateTime = product.StartDate ; ResetIndex = initLevel}]
+        }
+
+/// CONST
+
 // Min Reset Interval
 let MIN_RESET_INTERVAL = TimeSpan(0,1,0)
+
+/// FUNCTIONS
 
 // Create a standardized (midnight reset) product
 let createProduct (startD: DateTimeOffset) (nb: int) (gammaD: float) (thetaD: float) =
@@ -33,11 +62,45 @@ let createProduct (startD: DateTimeOffset) (nb: int) (gammaD: float) (thetaD: fl
         ThetaD = thetaD
     }
 
+
+// Calc Realised Gamma PnL
+// Complixity: O(n)
+let calcRealisedGamma (h: Holding) =
+    h.Resets
+    |> List.pairwise
+    |> List.sumBy (fun (x,y) -> 50. * h.Product.GammaD * pown (((y.ResetIndex - x.ResetIndex) / x.ResetIndex)) 2)
+
+// Calc Realised PnL
+let calcRealisedPnl (h: Holding) =
+    fun (dt: DateTimeOffset) ->
+        let nbDaysStarted = 
+            if dt >= h.Product.EndDate then h.Product.NbOfDays
+            else
+                Math.Floor(dt.Subtract(h.Product.StartDate).TotalDays + 1.)
+                |> int
+            
+        (calcRealisedGamma h) + (float nbDaysStarted) * h.Product.ThetaD
+
 //// TESTS
 // Test product creation
 let gammaD = 1825000.
 let thetaD = -250000.
-let nbDays = 2
+let nbDays = 5
 
 let product1 = createProduct (DateTimeOffset.UtcNow) nbDays gammaD thetaD
 
+let rnd = System.Random(Seed = 0)
+let randoms = [for i in 0..4 do yield rnd.NextDouble()]
+
+
+let resets =
+    List.init 5 (fun i -> DateTimeOffset.UtcNow.AddHours(float (8 * i)), float i)
+    |> List.map (fun (d,i) -> {DateTime = d ; ResetIndex = (50000. * (1.0 + i * 0.05)) |> round})
+
+let initH = Holding.Create(product1, 50000.)
+
+let calcRGamma = calcRealisedGamma (initH.addResets(resets))
+
+let calcRPnl = calcRealisedPnl (initH.addResets(resets))
+DateTimeOffset.UtcNow.AddHours(33.)
+|> calcRPnl
